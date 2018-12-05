4094ca1e9d1d8cf03100c6ef836ddeb3294aea13
Shane <shane@xs.vc>
2018-12-05 01:42:52 -0800

    added bootcamp

--- notetaking/settings.py
+++ notetaking/settings.py
@@ -12,7 +12,6 @@ import os
 from configurations import Configuration, values
 
 
-
 class Common(Configuration):
     # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
     BASE_DIR = os.path.dirname(os.path.dirname(__file__))
